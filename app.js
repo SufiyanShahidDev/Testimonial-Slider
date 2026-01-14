134541b11238c3af568339363c02e989199f1464
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-14 20:42:39 +0500

    Initial commit

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ function Review(img, name, job, info) {
     this.info = info;
 }
 
-let firest = new Review(
-    `https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/1775dd87258277f275233da15d412966~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=e5d0d695&x-expires=1768564800&x-signature=irs1o%2BBoX2KxN859JKMsRFlN2mQ%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my2`,
-    "Muhammad Arman Coder",
+let first = new Review(
+    `./asset/img1.jpg`,
+    "Arham Maqsood",
     "Web Developer",
     `- Clean and professional UI design
 - Use modern CSS (flexbox/grid, shadows, hover effects)
@@ -18,8 +18,8 @@ let firest = new Review(
 )
 
 let second = new Review(
-    `https://scontent.cdninstagram.com/v/t51.82787-19/608243164_17918228145244094_6790389802022099368_n.jpg?stp=dst-jpg_s206x206_tt6&_nc_cat=108&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=eAp24coxBoYQ7kNvwFKyIdE&_nc_oc=Adl8ImdSAT-ZYQEhjlIuZVaCZc9Q13sQncujy8AavUxokUydcGqAmtl5JzlNVD6CFuo&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=3wkM2adtalsaxPqo5-G7IQ&oh=00_AfoamLnB__ceUnoV8cEmM1h6tYYDeJLVPEETCLx4gHRmLQ&oe=696D660A`,
-    "Asad Coder",
+    `./asset/img2.jpg`,
+    "Nehal",
     "Web Dsigner",
     `-- Dynamic data handling using JavaScript objects/arrays
 - At least one interactive feature (buttons, toggle, slider, modal, etc.)
@@ -29,8 +29,8 @@ let second = new Review(
 
 
 let third = new Review(
-    `https://scontent.cdninstagram.com/v/t51.82787-19/608419954_17870674764500283_5766742047333100471_n.jpg?stp=dst-jpg_s206x206_tt6&_nc_cat=100&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=4ebzuWuYSoAQ7kNvwEXBRu7&_nc_oc=AdmkyZItVhUTvC4dFfdztutZhJKKId9BCN_sTtt-OC4LVhBcDt-BcH2JLIZKjmGOBj0&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=6MvysMhYFRpa2ZeyIj_PEQ&oh=00_AfrTQ9BHsxkU9g5pZYmjxFUmZqED8d-hSahab5ULy4kFYA&oe=696D4E8E`,
-    "Ahmed Khan",
+    `./asset/img3.jpg`,
+    "Umer",
     "AI & Data Scientist",
     `- Provide complete code for all files
 - Do not skip any part
@@ -38,7 +38,7 @@ let third = new Review(
 )
 
 
-let contruster = [firest, second, third];
+let contruster = [first, second, third];
 
 let imgs = document.getElementById("person-img");
 let author = document.getElementById("author");
